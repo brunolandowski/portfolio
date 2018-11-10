ec5a81b48c15cd5db827c6ed5bdd5c4ad5abf765
Bruno Landowski <brunolandowski@new-host.home>
2018-11-10 21:30:40 +0100

    pressure

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
           iArray.push('<a href="'+json.feed.entry[i].gsx$url.$t+ '">URL</a>');
           tableArray.push(iArray);
           
-          $("#container").append('<article><div class="content"><h2>' + json.feed.entry[i].gsx$name.$t + '</h2><h3>' + json.feed.entry[i].gsx$aside.$t + '</h3></div><div class="flip-box"><div class="flip-box-inner"><div class="flip-box-front"><video autoplay loop poster="' + json.feed.entry[i].gsx$urlimg.$t + '"> <source src="' + json.feed.entry[i].gsx$urlvid.$t + '" type="video/mp4" /></video></div><div class="flip-box-back" style="background-color:' + json.feed.entry[i].gsx$color.$t + ';"></div></div></div></article>');
+          $("#container").append('<a class="link" target="_blank"><article><div class="content"><h2>' + json.feed.entry[i].gsx$name.$t + '</h2><h3>' + json.feed.entry[i].gsx$aside.$t + '</h3></div><div class="flip-box"><div class="flip-box-inner"><div class="flip-box-front"><video autoplay loop poster="' + json.feed.entry[i].gsx$urlimg.$t + '"> <source src="' + json.feed.entry[i].gsx$urlvid.$t + '" type="video/mp4" /></video></div><div class="flip-box-back" style="background-color:' + json.feed.entry[i].gsx$color.$t + ';"></div></div></div></article></a>');
         
           
         }
@@ -55,9 +55,52 @@ $(document).ready(function() {
       })
        .done(function() {
         console.log( "second success" );
+        $("article").each(function( i ) {
+
+
+                // Regular
+          $(this).pressure({
+                start: function(event){
+                  // this is called on force start
+                  $(".couille").html("small THROAT");
+                  $(this).css("background", "red");
+                  $(this).addClass("hover");
+                },
+                end: function(){
+                  // this is called on force end
+                  $(this).removeClass("hover");
+                 
+                },
+                startDeepPress: function(event){
+                  // this is called on "force click" / "deep press", aka once the force is greater than 0.5
+                  
+                },
+                endDeepPress: function(){
+                  // this is called when the "force click" / "deep press" end
+                },
+                change: function(force, event){
+                  // this is called every time there is a change in pressure
+                  // force will always be a value from 0 to 1 on mobile and desktop
+                  if (force > 0.99) {
+                    $(".couille").html(i+1);
+                  }
+                 
+                },
+                unsupported: function(){
+                  // NOTE: this is only called if the polyfill option is disabled!
+                  // this is called once there is a touch on the element and the device or browser does not support Force or 3D touch
+                }
+              });
+         
+        });
+
+
+
       })
       ;
     
 });
+
+
 
 
